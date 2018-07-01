fa8f9b075e11aa335c73cd072a3113df325495d7
Conrad Knapp <knappco@bc.edu>
2018-07-01 12:48:01 -0400

    Create handleSubmit function

--- src/Form.js
+++ src/Form.js
@@ -1,3 +1,5 @@
+import axios from "axios";
+
 const API_URL = "https://api.github.com/users";
 
 class Form {
@@ -9,13 +11,23 @@ class Form {
     this.searchInput.addEventListener("keyup", () => this.handleKeyup(event));
 
     this.submitButton = document.querySelector('button[type="submit"]');
-    // !"" -> true, !"term" -> false
     this.submitButton.disabled = !this.searchTerm;
+
+    this.form = document.querySelector("form");
+    this.form.addEventListener("submit", () => this.handleSubmit(event));
   }
   handleKeyup(event) {
     this.searchTerm = event.target.value.trim();
     this.API_URL = `${API_URL}/${this.searchTerm}`;
     this.submitButton.disabled = !this.searchTerm;
+  }
+  handleSubmit(event) {
+    event.preventDefault();
+    axios
+      .get(this.API_URL)
+      .then(({ data }) => console.log(data))
+      .catch(err => console.error("Promise rejected!", err));
+    this.form.reset();
   }
 }
 
