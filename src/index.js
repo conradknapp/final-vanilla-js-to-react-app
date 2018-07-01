846d0567358c71931b30ac6f50bdfd821bb0e2e1
Conrad Knapp <knappco@bc.edu>
2018-07-01 14:28:41 -0400

    Add getCards method to App constructor

--- src/index.js
+++ src/index.js
@@ -3,17 +3,25 @@ import CardList from "./CardList";
 
 class App {
   constructor() {
-    this.cards = [];
+    this.cards = this.getCards();
     this.addCard = this.addCard.bind(this);
     this.clearCards = this.clearCards.bind(this);
   }
   addCard(data) {
     this.cards = [data, ...this.cards];
     CardList(this.cards);
+    localStorage.setItem("users", JSON.stringify(this.cards));
   }
   clearCards() {
     this.cards = [];
     CardList(this.cards);
+  }
+  getCards() {
+    if (localStorage.getItem("users")) {
+      return JSON.parse(localStorage.getItem("users"));
+    } else {
+      return [];
+    }
   }
 }
 
@@ -21,3 +29,4 @@ const app = new App();
 const form = new Form(app.addCard, app.clearCards);
 
 export const render = (html, node) => (node.innerHTML = html);
+CardList(app.cards);
